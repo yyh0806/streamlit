05811d62d0460dcc72175783972e380ded364f97
yang yuhui <yangyuhui0806@gmail.com>
2021-03-20 22:37:09 +0900

    init

--- setup.sh
+++ setup.sh
@@ -2,10 +2,14 @@
 mkdir -p ~/.streamlit/
  
  
- 
+echo "\
+[general]\n\
+email = \"yangyuhui0806@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+" > ~/.streamlit/config.toml
